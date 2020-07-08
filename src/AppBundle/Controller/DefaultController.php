bee18bc76a3483afa2c3b04e9845888191bd59e2
Muhammed Najmul Huda <del.rony@yahoo.com>
2020-07-08 17:15:00 +0000

    Add bootstrap and jquery

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,14 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $em = $this->getDoctrine()->getManager();
+
+        $contents = $em->getRepository('AppBundle\Entity\Content')
+            ->findAll();
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'contents' => $contents
         ]);
     }
 }
